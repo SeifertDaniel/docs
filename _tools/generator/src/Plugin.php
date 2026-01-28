ff21b27877a98ee78c8bc8f2777d7b70aa014595
Daniel Seifert <git@daniel-seifert.com>
2026-01-28 15:52:46 +0100

    add plugin dto

---
+++ _tools/generator/src/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Docs\Generator;
+
+final class Plugin
+{
+    public string $slug;
+    public string $name;
+
+    /** @var list<SemVersion> */
+    public array $versions;
+
+    public SemVersion $latest;
+    public \DateTimeImmutable $updatedAt;
+
+    /**
+     * @param list<SemVersion> $versions
+     */
+    public function __construct(
+        string $slug,
+        string $name,
+        array $versions,
+        \DateTimeImmutable $updatedAt
+    ) {
+        $this->slug = $slug;
+        $this->name = $name;
+        $this->versions = $versions;
+        $this->latest = $versions[0];
+        $this->updatedAt = $updatedAt;
+    }
+}
